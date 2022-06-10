7b657aa39f3c4cd772bfc08dc94e42f19a3d42d5
clickjaw <clickjaw@outlook.com>
2022-06-10 15:33:47 -0500

    I got nowhere.

--- js/app.js
+++ js/app.js
@@ -8,12 +8,17 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-// function sum(a, b) { //eslint-disable-line
+function sum(a, b) { //eslint-disable-line
+let mySumArray = [];
+let mySum = a + b;
+let mySumString = `The sum of ${a} and ${b} is ${mySum}.`;
+mySumArray.push(mySum,mySumString);
+return mySumArray;
 
-// }
-
+}
+console.log(sum());
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -34,8 +39,8 @@ myMultiplyArray.push(myMultiply,myMultiplyString);
 return myMultiplyArray;
 
 }
-document.write(multiply());
-// Here is the test for multiply(); uncomment it to run it
+console.log(multiply());
+    // Here is the test for multiply(); uncomment it to run it
 testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
@@ -53,8 +58,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+let myThreeSumArray = [];
+let myThreeSum
+let myThreeMultiplyArray = [];
 
+
+return myThreeMultiplyArray;
+return myThreeSumArray;    
 }
+
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
 // testSumAndMultiply(4,7,5);
